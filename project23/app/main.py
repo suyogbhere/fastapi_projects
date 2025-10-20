cf22cc019dfc9629437b72f7867f99b6a19128db
Suyog Bhere <suyogbhere55@gmail.com>
2025-10-20 21:51:46 +0530

    userprofile with file upload

---
+++ project23/app/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.responses import HTMLResponse
+from typing import Annotated
+import os
+import shutil
+
+
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def get_form():
+    return """
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>user profile Upload</title>
+        </head>
+        <body>
+            <form action="/user-with-file/" method="post" enctype="multipart/form-data">
+                <label for="username">Username:</label> <br>
+                <input type="text" id="username" name="username" required> <br><br>
+                <label for="file"> Profile Picture (Optional):</label> <br>
+                <input type="file" id="file" name="file" accept="image/"> <br> <br>
+                <input type="submit" value="Submit">
+            </form>
+        </body>
+    </html>"""  
+
+
+
+@app.post("/user-with-file/")
+async def create_user_with_file(
+    username:Annotated[str, Form()],
+    file: Annotated[UploadFile | None, File()]=None
+):
+    response = {"username": username}
+    if file:
+        save_path = f"uploads/{file.filename}"
+        os.makedirs("uploads",exist_ok=True)
+        with open(save_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        response["filename"] = file.filename
+    return response
