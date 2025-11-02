a73f883af69634e424e0640eddf6c8202e70a744
Suyog Bhere <suyogbhere55@gmail.com>
2025-11-02 22:44:34 +0530

    Group By

--- project28/main.py
+++ project28/main.py
@@ -1,6 +1,41 @@
-from tables import create_table
+from tables import create_tables
+from services import *
 
 
-## Create Table
-create_table()
+#Create Table
+create_tables()
 
+### Create Data
+# create_user("suyog", "suyog@johomail.com")
+# create_user("Suraj", "suraj@johomail.com")
+# create_user("Anand", "ananad@gmail.com")
+# create_user("Prashant", "prashant@gmail.com")
+# create_post(1,"Hellow World", "This is suyog first post")
+# create_post(2,"Hellow World", "This is Suraj first post")
+# create_post(3,"Hi Hello", "This is Networking lelated posts")
+
+
+
+
+## Read Data
+# get_user_by_id(1)
+# print(get_user_by_id(1))
+# print(get_all_users())
+# print(get_posts_by_user(2))
+
+
+
+## Update Data
+# update_user_email(1,'suyogbhere@gmail.com')
+
+
+
+# Delete Data
+# delete_post(2)
+
+
+
+## Get Users data Order by
+# print(get_users_ordered_by_name())
+# print(get_latest_post_first())
+print(get_post_count_per_user())
--- project28/tables.py
+++ project28/tables.py
@@ -1,19 +1,18 @@
+#To Create Table 
+
 from db import engine
-from sqlalchemy import MetaData, Table,Column, Integer, String, ForeignKey
+from sqlalchemy import MetaData, Table, Column, Integer, String, DateTime, ForeignKey
 
 metadata = MetaData()
 
-
-## Users Table
+# user Table
 users = Table(
     "users",
     metadata,
-    Column("id", Integer, primary_key= True),
-    Column("name", String(length=50),nullable=False),
+    Column("id", Integer, primary_key=True),
+    Column("name", String(length=50), nullable=False),
     Column("email", String, nullable=False, unique=True),
 )
-
-
 
 ## Relationaship  One to Many
 posts = Table(
@@ -26,8 +25,42 @@ posts = Table(
 )
 
 
-## Create Table in Database
-# metadata.create_all(engine)
+## Relationaship  One to one
+profile = Table(
+    "profile" , 
+    metadata,
+    Column("id", Integer, primary_key = True) ,
+    Column("user_id", Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False, unique=True),
+    Column("bio", String, nullable=False),
+    Column("address",String, nullable=False),
+)
 
-def create_table():
+
+
+# Relationship many to many
+address = Table(
+    "address" , 
+    metadata,
+    Column("id", Integer, primary_key = True) ,
+    Column("street", String, nullable=False),
+    Column("country",String, nullable=False),
+)
+
+
+#Userr address association
+user_address_association = Table(
+    "user_address_association",
+    metadata,
+    Column("user_id", Integer, ForeignKey("users.id",
+    ondelete="CASCADE"), primary_key=True),
+    Column("address_id", Integer, ForeignKey("address.id",
+    ondelete="CASCADE"), primary_key=True)
+)
+
+
+
+## Create table in Database
+def create_tables():
     metadata.create_all(engine)
+
+
