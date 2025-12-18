78d3fce76d404a7bd25b02188b14db2e821203f2
Suyog Bhere <suyogbhere55@gmail.com>
2025-12-18 23:02:25 +0530

    new project update

--- project32/models.py
+++ project32/models.py
@@ -12,25 +12,36 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = "users"
 
-    id = Mapped[int] =mapped_column(primary_key=True)
-    name = Mapped[int] = mapped_column(String(50), nullable=False)
-    email = Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    id : Mapped[int] = mapped_column(primary_key=True)
+    name : Mapped[int] = mapped_column(String(50), nullable=False)
+    email : Mapped[str] = mapped_column(String, nullable=False, unique=True)
 
     ## One to Many : User to Post
-    posts = Mapped[list["Post"]] = relationship("Post", back_populates="user", cascade="all, delete")
+    posts : Mapped[list["Post"]] = relationship("Post", back_populates="user", cascade="all, delete")
 
     def __repr__(self) -> str:
-        return f"<User{id={self.id}, name={self.name}, email={self.email}}>"
+        return f"<User id={self.id} name={self.name} email={self.email}>"
 
 
 ## Post Model One-to-Many
 class Post(Base):
+    __tablename__ = "posts"
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete='CASCADE'), nullable=False)
     title: Mapped[str] = mapped_column(String, nullable=False)
     content: Mapped[str] = mapped_column(String, nullable=False)
 
-    user = Mapped["User"] = relationship("User", back_populates='posts')
+    user : Mapped["User"] = relationship("User", back_populates='posts')
 
     def __repr__(self) -> str:
-        return f"Post id={self.id} title={self.id}"
+        return f"Post id={self.id} title={self.title}"
+
+
+## Create Table
+def create_tables():
+    Base.metadata.create_all(engine)
+
+
+##Drop Table
+def drop_tables():
+    Base.metadata.drop_all(engine)
---
+++ project32/services.py
@@ -0,0 +1,6 @@
+# from models import *
+# from db import sessionlocal
+
+
+# ## Insert or Create User
+# def create_user(name:str, email:str)
