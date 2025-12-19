238a05ac5a4973a048ed3fbbe3fc942994400679
Suyog Bhere <suyogbhere55@gmail.com>
2025-12-19 23:06:09 +0530

    new things

--- project32/main.py
+++ project32/main.py
@@ -1,7 +1,17 @@
 from models import create_tables, drop_tables
+from services import *
+
+## Create Table
+# create_tables()
 
 
-create_tables()
+
+# Create Data
+# result = create_user("suyog", "suyog@gmail.com")
+# print(result)
+
+
+create_post(1,"Python", "Python Language is easy to use")
 
 
 # drop_tables()
--- project32/services.py
+++ project32/services.py
@@ -1,6 +1,21 @@
-# from models import *
-# from db import sessionlocal
+from models import *
+from db import sessionlocal
 
 
-# ## Insert or Create User
-# def create_user(name:str, email:str)
+## Insert or Create User
+def create_user(name:str, email:str):
+    with sessionlocal() as session:
+        user = User(name=name,email=email)
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        return user
+    
+
+
+def create_post(user_id: int, title: str, content: str):
+    with sessionlocal() as session:
+        post = Post(user_id= user_id, title = title, content=content)
+        session.add(post)
+        session.commit()
+    
