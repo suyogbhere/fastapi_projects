cc3e0fd712b44e7240bf8d38da3a29934711b81b
Suyog Bhere <suyogbhere55@gmail.com>
2026-03-19 22:27:33 +0530

    checking git diff and other thing

--- taskkaro_v2/app/main.py
+++ taskkaro_v2/app/main.py
@@ -50,7 +50,7 @@ def task_patch(task_id: int, new_task:Taskpatch):
 
 
 
-@app.delete("/task/{task_id}")
+@app.delete("/task/{task_id}", response_model=Taskout)
 def task_delete(task_id: int):
     task = delete_task(task_id)
     return task
